b4c7016ae71c64918ff91cecbf61586e5630eea0
Brian Alvarez <brianalvarez2000@gmail.com>
2025-02-21 08:08:32 -0500

    seccion 5 OK

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  title: "Fernando' Blog",
+  blog: "Blog de Fernando Herrera"
+}
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    image: image(),
+    author: z.string(),
+    tags: z.array(z.string()),
+  })
+})
+
+export const collections = {
+  blog: blogCollection
+}
